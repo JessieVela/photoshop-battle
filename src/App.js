bb38d46e97d98564c311fdcb8491adbdc5d9c502
Jessie Vela <jessievela45@gmail.com>
2020-03-25 14:19:28 -0700

    Added card design

--- src/App.js
+++ src/App.js
@@ -3,13 +3,19 @@ import './App.css';
 import Navigation from './components/Navigation'
 import { Container } from 'react-bootstrap';
 import FAQ from './components/FAQ';
+import CardDetails from './components/CardDetails'
 
 function App() {
   return (
     <div className="App">
       <Container>
-          <Navigation />
-          <FAQ />
+        <Navigation />
+      </Container>
+      <Container>
+        <CardDetails url="https://external-preview.redd.it/GPEmX1RaDKgT2LPhN9aL4CS6UE5NehVg19CsHDtISH8.jpg?width=640&crop=smart&auto=webp&s=bc337fede8fa209b9008868ead4d524a1a2e5490" />
+        <CardDetails url="https://preview.redd.it/5jmztfwzopo41.jpg?width=640&crop=smart&auto=webp&s=a3b5940708071cde9a442da2923f58b28029f0c6" />
+        <CardDetails url="https://preview.redd.it/q5iobba3nso41.jpg?width=640&crop=smart&auto=webp&s=d05686da0f2b961cd119c1e0c6485c9423962653" />
+        <CardDetails url="https://preview.redd.it/fnhgu3m8uso41.jpg?width=640&crop=smart&auto=webp&s=19f9048f13a20dfe8f2810722feffa0cd88db85e" />
       </Container>
     </div>
   );
---
+++ src/components/CardDetails.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import { Row, Col, Image } from 'react-bootstrap';
+import '../styles/CardDetails.css';
+
+export default class CardDetails extends Component {
+
+  render() {
+    return (
+      <Row className="card-detail-style">
+        <Col sm={4}>
+          <div className="image-div">
+            <Image thumbnail src={this.props.url} />
+          </div>
+        </Col>
+        <Col sm={8}>
+          <h2>Title of Photoshop Battle</h2>
+          <hr />
+          <p>Submissions: 6</p>
+          <p>Date: 25 Mar 2020</p>
+          <hr />
+          <span className="submission-link"><a href="#">See Submissions -></a></span>
+        </Col>
+      </Row>
+    )
+  }
+}
