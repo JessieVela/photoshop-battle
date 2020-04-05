cbe4c0a78e7ef25b479ac3bce4ee5070652e79ba
Jessie Vela <jessievela45@gmail.com>
2020-04-05 10:53:13 -0700

    remove redux from branch

--- src/index.js
+++ src/index.js
@@ -3,21 +3,11 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import { createStore } from "redux";
-import allReducer from "./reducers/index.js";
-import { Provider } from "react-redux";
-
-const store = createStore(
-  allReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
 
 ReactDOM.render(
-  <Provider store={store}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </Provider>,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById("root")
 );
 
